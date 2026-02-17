205ebfa49f4d1c9200d2bd39704e1238a22a6061
jd <jdsanchez2011@gmail.com>
2026-02-17 08:07:26 -0700

    move `provideAuth0` to app.config.ts

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,12 +4,21 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
+import { environment } from '../environments/environment';
+import { provideAuth0 } from '@auth0/auth0-angular';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    provideAuth0({
+      domain: environment.auth0.domain,
+      clientId: environment.auth0.clientId,
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
     providePrimeNG({
       theme: {
         preset: Aura
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,21 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { mergeApplicationConfig } from '@angular/core';
-import { provideAuth0 } from '@auth0/auth0-angular';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { environment } from './environments/environment';
 
-const auth0Config = mergeApplicationConfig(appConfig, {
-  providers: [
-    provideAuth0({
-      domain: environment.auth0.domain,
-      clientId: environment.auth0.clientId,
-      authorizationParams: {
-        redirect_uri: window.location.origin
-      }
-    })
-  ]
-});
-
-bootstrapApplication(App, auth0Config)
+bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
