ebfa433f2881bad1b7104a3db5dd19fb599ef069
jd <jdsanchez2011@gmail.com>
2026-03-03 13:25:03 -0700

    sakai-ng layout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppLayout } from './layout/component/app.layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AppLayout,
+    }
+];
