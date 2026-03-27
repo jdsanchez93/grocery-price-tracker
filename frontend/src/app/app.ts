a8ddbe3b2022add26aa9778eaa0fa69adb246a88
jd <jdsanchez2011@gmail.com>
2026-03-27 13:34:27 -0600

    more elegant loading - background color in index.html to match default theme - loading spinners in index.html and app component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class App {}
+export class App {
+  private auth = inject(AuthService);
+  isLoading = toSignal(this.auth.isLoading$, { initialValue: true });
+}
